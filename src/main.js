99a8978dff3e64bb877bc606074098ac1ec2cf63
samir-ben <samir-69@hotmail.fr>
2019-02-09 14:58:59 +0100

    feat: alert component
    - created alert component
    - add component globaly in app

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index'
 import '@/firebase/init';
+import Alert from './components/Shared/Alert.vue';
 
 
 Vue.config.productionTip = false
-
-
+Vue.component('app-alert', Alert);
 
   new Vue({
     router,
